672e032f9132e5d9b33dedacb0cccf2579f57b92
sathya-pramodh <sathyapramodh17@gmail.com>
2024-08-26 22:18:33 +0530

    feat: Add Flask API and basic endpoints.

---
+++ api/main.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify
+
+from utils.hand_gesture import generate_hand_gesture
+from utils.head_gesture import generate_head_gesture
+
+app = Flask(__file__)
+
+
+@app.route("/api/get-hand-gesture-prompt", methods=["GET"])
+def get_hand_gesture_prompt():
+    """
+    # Methods: ```GET```
+
+    # Return:
+    ```json
+    {
+        "gesture": <description of gesture>
+    }
+    ```
+
+    # Example:
+    ```json
+    {
+        "gesture": "Pinch your fingers together slowly."
+    }
+    ```
+    """
+    gesture = generate_hand_gesture()
+    return jsonify({"gesture": gesture})
+
+
+@app.route("/api/get-head-gesture-prompt", methods=["GET"])
+def get_head_gesture_prompt():
+    """
+    # Methods: ```GET```
+
+    # Return:
+    ```json
+    {
+        "gesture": <description of gesture>
+    }
+    ```
+
+    # Example:
+    ```json
+    {
+        "gesture": "Frown and then relax your face."
+
+    }
+    ```
+    """
+    gesture = generate_head_gesture()
+    return jsonify({"gesture": gesture})
+
+
+if __name__ == "__main__":
+    app.run()
